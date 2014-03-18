bcc8b582b58bb695412b88505615bde3174608a9
jesse keane <me@jessekeane.me>
2014-03-17 18:07:20 -0700

    use smtp to send, update deps

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 var smtp = require('smtp-protocol')
-  , sendmail = require('sendmail')()
   , concat = require('concat-stream')
 
 module.exports = ricochet
 
-function ricochet(alias_lookup) {
+function ricochet(domain, alias_lookup) {
   var server = smtp.createServer(request_response)
 
   return server
 
   function request_response(req) {
-    var alias_address
+    var to_alias
 
     req.on('to', to_response)
     req.on('message', message_response)
@@ -20,34 +19,37 @@ function ricochet(alias_lookup) {
 
       function determine(err, alias) {
         if (err || !alias) return ack.reject()
-        alias_address = alias
+        to_alias = alias
 
         ack.accept()
       }
     }
-    function message_response(stream, ack) {
-      stream.pipe(concat(send_mail))
-      ack.accept()
-    }
 
-    function send_mail(data) {
+    function message_response(stream, ack) {
       var bits = data.split('\n')
         , subject = bits[0]
         , content = bits.slice(1).join('\n')
 
-      sendmail(
-          {
-              from: req.from
-            , to: alias_address
-            , subject: subject
-            , content: content
-          }
-        , check_success
-      )
-    }
+      var address = req.from.split('@')
+        , user = address[0]
+        , host = address[1]
 
-    function check_success(err, reply) {
-      if (err) console.error(err)
+      alias_lookup(req.from, setup_client)
+
+      function setup_client(err, from_alias) {
+        smtp.connect(host, 25, send_mail)
+
+        function send_mail(mail) {
+          mail.helo(domain)
+          mail.from(from_alias)
+          mail.to(to_alias)
+          mail.data()
+          stream.pipe(mail.message())
+          
+          ack.accept()
+          mail.quit()
+        }
+      }
     }
   }
 }
