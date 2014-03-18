cc1d676eb1fdccd83d6a0c394b8295274e0882ea
jesse keane <me@jessekeane.me>
2014-03-17 22:00:54 -0700

    zomg

--- index.js
+++ index.js
@@ -8,10 +8,26 @@ function ricochet(domain, alias_lookup) {
   return server
 
   function request_response(req) {
-    var to_alias
+    var from_alias
+      , to_alias
 
+    req.on('greeting', function(cmd, ack) {
+      ack.accept(250, domain + '- relay server')
+    })
+    req.on('from', from_response)
     req.on('to', to_response)
     req.on('message', message_response)
+
+    function from_response(from, ack) {
+      alias_lookup(from, determine)
+
+      function determine(err, alias) {
+        if(err || !alias) return ack.reject()
+        from_alias = alias
+
+        ack.accept(250, domain)
+      }
+    }
 
     function to_response(to, ack) {
       alias_lookup(to, determine)
@@ -20,34 +36,22 @@ function ricochet(domain, alias_lookup) {
         if (err || !alias) return ack.reject()
         to_alias = alias
 
-        ack.accept()
+        ack.accept(250, domain)
       }
     }
 
     function message_response(stream, ack) {
-      var bits = data.split('\n')
-        , subject = bits[0]
-        , content = bits.slice(1).join('\n')
+      smtp.connect(to_alias.split('@')[1], 25, send_mail)
 
-      var address = req.from.split('@')
-        , user = address[0]
-        , host = address[1]
+      function send_mail(mail) {
+        mail.helo(domain)
+        mail.from(from_alias)
+        mail.to(to_alias)
+        mail.data()
+        stream.pipe(mail.message())
 
-      alias_lookup(req.from, setup_client)
-
-      function setup_client(err, from_alias) {
-        smtp.connect(host, 25, send_mail)
-
-        function send_mail(mail) {
-          mail.helo(domain)
-          mail.from(from_alias)
-          mail.to(to_alias)
-          mail.data()
-          stream.pipe(mail.message())
-          
-          ack.accept()
-          mail.quit()
-        }
+        ack.accept()
+        mail.quit()
       }
     }
   }
